c149a8c93456afabc852c1336914ea3acaf60ca1
prokash <pprokash102@gmail.com>
2022-05-13 23:26:28 +0600

    avalibe service filter and fend get api create

--- index.js
+++ index.js
@@ -28,6 +28,27 @@ const run = async () => {
       const services = await cursor.toArray();
       res.send(services);
     });
+    // available service get data
+    app.get("/available", async (req, res) => {
+      const date = req.query.date;
+      // step 1
+      const services = await serviceCollection.find().toArray();
+      // step 2
+      const query = { date: date };
+      const booking = await bookingCollection.find(query).toArray();
+      //step 3 loop
+      services.forEach((service) => {
+        const serviceBookings = booking?.filter(
+          (book) => book.treatment === service.name
+        );
+        const bookedSlot = serviceBookings?.map((book) => book.slot);
+        const availableSlot = service.slots.filter(
+          (slot) => !bookedSlot.includes(slot)
+        );
+        service.slots = availableSlot;
+      });
+      res.send(services);
+    });
     // booking post api create
     app.post("/booking", async (req, res) => {
       const booking = req.body;
